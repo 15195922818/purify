1be761cdb1db5718e1e9908f6a7da217cc64214b
15195922818 <542548496@qq.com>
2018-08-29 20:06:26 +0800

    Initial commit

---
+++ purify/src/test/java/com/hansight/analysis/purify/PurifyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hansight.analysis.purify;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class PurifyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
